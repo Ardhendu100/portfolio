0bcaa5cd4663d74a9b91602ddfcb5268f7e40d32
Ardhendu <ardhendu.s@auroinfo.com>
2026-03-03 12:05:34 +0530

    updated

--- src/components/Chatbot/ChatPopup.js
+++ src/components/Chatbot/ChatPopup.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { HiX, HiChat, HiPaperAirplane } from "react-icons/hi";
 import { BsRobot } from "react-icons/bs";
